0920e3ff4bc5c64170a237eabbdc48ad8b580d0a
oussama4707 <104109642+oussama4707@users.noreply.github.com>
2026-08-07 02:53:26 +0100

    Modify search function to limit results to one

--- search_track.py
+++ search_track.py
@@ -5,11 +5,10 @@ yt = YTMusic()
 def search_youtube_music_ids(queries):
     ids = []
     for q in queries:
-        res = yt.search(q, filter="songs")
-        if res and len(res) > 0 and 'videoId' in res[0]:
-            ids.append(res[0]['videoId'])
+        res = yt.search(q, filter="songs", limit=1)
+        if res:
+            ids.append(res[0].get('videoId'))
         else:
             ids.append(None)
         
     return ids
-
